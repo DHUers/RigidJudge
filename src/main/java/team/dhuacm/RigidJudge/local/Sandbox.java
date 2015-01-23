f5e5ce70a1c7047ed1290598248a5eb2a1d4131a
wujysh <wujysh@gmail.com>
2015-01-23 11:05:40 +0800

    Java Native Interface (JNI)

--- src/main/java/team/dhuacm/RigidJudge/local/Sandbox.java
+++ src/main/java/team/dhuacm/RigidJudge/local/Sandbox.java
@@ -4,4 +4,15 @@ package team.dhuacm.RigidJudge.local;
  * Created by wujy on 15-1-23.
  */
 public class Sandbox {
+    native void Sandbox();
+    static {
+        System.loadLibrary("sandbox");
+    }
+    static public void main(String argv[]) {
+        Sandbox sandbox = new Sandbox();
+        sandbox.init();
+    }
+
+    private void init() {
+    }
 }
