02349d7908131505e9326c68a8cfccddef2cba0f
wujysh <wujysh@gmail.com>
2015-01-23 11:22:03 +0800

    generate JNI header

--- src/main/java/team/dhuacm/RigidJudge/local/Sandbox.java
+++ src/main/java/team/dhuacm/RigidJudge/local/Sandbox.java
@@ -4,15 +4,13 @@ package team.dhuacm.RigidJudge.local;
  * Created by wujy on 15-1-23.
  */
 public class Sandbox {
-    native void Sandbox();
     static {
         System.loadLibrary("sandbox");
     }
-    static public void main(String argv[]) {
-        Sandbox sandbox = new Sandbox();
-        sandbox.init();
-    }
 
-    private void init() {
+    private native void init();
+
+    public static void main(String[] args) {
+        new Sandbox().init();
     }
 }
