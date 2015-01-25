d2c237d26d7d368745087e8339fb59f52d3311dd
wujysh <wujysh@gmail.com>
2015-01-25 14:07:34 +0800

    add Clean after local judge

--- src/main/java/team/dhuacm/RigidJudge/local/Clean.java
+++ src/main/java/team/dhuacm/RigidJudge/local/Clean.java
@@ -3,13 +3,34 @@ package team.dhuacm.RigidJudge.local;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 /**
  * Created by wujy on 15-1-25.
  */
 public class Clean {
     private final static Logger logger = LoggerFactory.getLogger(Clean.class.getSimpleName());
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static boolean doClean() {
+        if (deleteDir(new File("tmp"))) {
+            logger.info("Success.");
+        } else {
+            logger.error("fail.");
+        }
         return true;
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (String child : children) {
+                boolean success = deleteDir(new File(dir, child));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
     }
 }
